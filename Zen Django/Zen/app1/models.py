3913f25c85038c3e576bc0ca6f4a19a8ce5329a2
anweshb <54733708+anweshb@users.noreply.github.com>
2019-11-07 01:28:52 +0530

    Update models.py

--- Zen Django/Zen/app1/models.py
+++ Zen Django/Zen/app1/models.py
@@ -5,22 +5,10 @@ from django.conf import settings
 
 # Create your models here.
 
-class Doctor(AbstractBaseUser):
-    first_name = models.CharField(default="",max_length=30,required=True)
-    last_name = models.CharField(default="",max_length=30,required=True)
-    email_id = models.EmailField(max_length=40,unique=True)
-    username = models.CharField(max_length=30,unique=True)
+class Doctor(models.Model):
+    user = models.OneToOneField(User,on_delete='models.CASACDE')
     phone_no = models.IntegerField(unique=True)
-    exp = models.IntegerField(choices=[('0','0'),('2+','2+'),('5+','5+'),('10+','10+')])
-    date_joined = models.DateTimeField(verbose_name = 'date joined',auto_now_add = True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.username
-
+    user_type = models.TextField(default='Doctor')
 
 class Feedback(models.Model):
     name = models.CharField(max_length=120)
@@ -29,7 +17,7 @@ class Feedback(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
 class Responses1(models.Model):
     username = models.TextField(default="")
